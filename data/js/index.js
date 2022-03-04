83be546cad7838493f7dadb273aa2f1b63ecdf28
Diablolend <Diablolend@gmail.com>
2022-03-05 02:52:34 +0500

    v 0.14.8

--- data/js/index.js
+++ data/js/index.js
@@ -1,3 +1,15 @@
-//document.createElement('aside');
-//document.createElement('article');
-// alert('Привет-привет! Модуль JS успешно подгружен!')
+// Основной JavaScript практического сайта.
+
+// alert('Привет-привет! Основной модуль JS успешно подгружен!')
+
+// Плавные анимации для кнопок скролла вверх и вниз.
+function ScrollUp(){
+    var t,s;
+    s=document.body.scrollTop||window.pageYOffset;
+    t=setInterval(function(){if(s>0)window.scroll(0,s-=5);else clearInterval(t)},5);
+}
+function ScrollDown(){
+    var t,s,d;
+    s=document.body.scrollTop||window.pageYOffset;
+    t=setInterval(function(){if(s>=0)window.scroll(0,s+=5);else clearInterval(t)},5);
+}
