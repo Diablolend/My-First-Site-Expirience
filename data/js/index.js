004c8b77788688797e8b75837804c03f9138eba2
Diablolend <Diablolend@gmail.com>
2022-03-10 22:57:30 +0500

    v 0.14.9

--- data/js/index.js
+++ data/js/index.js
@@ -9,7 +9,31 @@ function ScrollUp(){
     t=setInterval(function(){if(s>0)window.scroll(0,s-=5);else clearInterval(t)},5);
 }
 function ScrollDown(){
-    var t,s,d;
+    var t,s;
     s=document.body.scrollTop||window.pageYOffset;
     t=setInterval(function(){if(s>=0)window.scroll(0,s+=5);else clearInterval(t)},5);
 }
+
+// Замена css tabs. Jeb mosniy. Переключает вкладки без отдельных страниц.
+function showTab(tabs, tab){
+    var links = tabs.querySelectorAll(".my-tabs-link > a")
+    links.forEach(function(link) {
+        console.log(link.getAttribute("href"))
+        document.querySelector(link.getAttribute("href")).style.display = 'none';
+    });
+    document.querySelector(tab.getAttribute("href")).style.display = 'block';
+}
+
+document.addEventListener('DOMContentLoaded', function(){ 
+    var my_tabs_array = document.querySelectorAll(".my-tabs");
+    
+    my_tabs_array.forEach(function(tabs) {
+        var links = tabs.querySelectorAll(".my-tabs-link > a")
+        links.forEach(function(link) {
+            link.addEventListener("click", function(event){
+                showTab(tabs, this)
+                event.preventDefault()
+            });
+        });
+    });
+});
