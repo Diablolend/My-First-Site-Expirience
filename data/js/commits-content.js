8d5173c002ca7a2b6c1965abefcb2a384cec125c
Diablolend <Diablolend@gmail.com>
2022-03-14 12:35:43 +0500

    v 0.15.5

--- data/js/commits-content.js
+++
@@ -1,3 +0,0 @@
-var link = document.querySelector('link[rel=import]');
-var content = link.import.querySelector('#repo-content-pjax-container > div.js-navigation-container.js-active-navigation-container.mt-3');
-document.body.appendChild(content.cloneNode(true));
