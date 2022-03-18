fadfc1320e9904e22a432f527bbc3513ad21d860
Diablolend <Diablolend@gmail.com>
2022-03-18 23:51:53 +0500

    v 0.15.8

--- data/js/index.js
+++ data/js/index.js
@@ -14,6 +14,18 @@ function ScrollDown(){
     t=setInterval(function(){if(s<document.documentElement.scrollHeight-document.documentElement.clientHeight)window.scroll(0,s+=5);else clearInterval(t)},5);
 }
 
+// Генерация таблицы
+function CreateTablePreset() {
+    document.write ('<table width="100%" border="1">');
+    for (i=1; i<6; i++) {
+    document.writeln("<tr>");
+        for (j=1; j<6; j++) document.write("<td>" + i + j + "<\/td>");
+        document.writeln("<\/tr>");
+    }
+    document.write ("<\/table> "); 
+    //alert('Таблица создана!')
+    }
+
 // Замена css tabs. Jeb mosniy. Переключает вкладки без отдельных страниц.
 function showTab(tabs, tab){
     var links = tabs.querySelectorAll(".my-tabs-link > a")
